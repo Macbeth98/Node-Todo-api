eb031ec0d61a3e0ae53f9f0410136588ad1675e8
Mani Chandra Teja <manichandra.teja@gmail.com>
2018-08-08 01:30:05 +0530

    Add GET?users/me authenticate middleware

--- server/models/user.js
+++ server/models/user.js
@@ -36,6 +36,7 @@ var UserSchema = new mongoose.Schema({
     }]
 });
 
+//Override method--> what to send when a mongoose model is converted to JSON
 UserSchema.methods.toJSON = function () {
   var user =this;
   var userObject = user.toObject();
@@ -52,6 +53,26 @@ UserSchema.methods.generateAuthToken = function () {
 
   return user.save().then(()=>{
     return token;
+  });
+};
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try{
+    decoded = jwt.verify(token,'abc123');
+  }catch(e){
+    // return new Promise((resolve,reject)=>{
+    //   reject();
+    // });
+    return Promise.reject();
+  }
+
+  return User.findOne({
+     '_id': decoded._id,
+    'tokens.token':token,
+    'tokens.access': 'auth'
   });
 };
 
