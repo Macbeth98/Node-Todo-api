558c57978a66d0f89ee771c65111a27662f8c6e4
Mani Chandra Teja <manichandra.teja@gmail.com>
2018-08-08 18:51:11 +0530

    Password hashing

--- play/hashing.js
+++ play/hashing.js
@@ -1,15 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data={
-  id:10
-};
+var password= '123abc!';
 
-var token = jwt.sign(data,'123abc');
-console.log(token);
+bcrypt.genSalt(10,(err,salt)=>{
+  bcrypt.hash(password,salt,(err,hash)=>{
+    console.log(hash);
+  });
+});
 
-var decoded = jwt.verify(token,'123abc');
-console.log(decoded);
+var hashed = '$2a$10$SP7NfnuMiFBWZh3t3USvKeCzPG4L.zrsuDAhkexW4jxjSVfr5eTIG';
+
+bcrypt.compare(password, hashed, (err,result)=>{
+  console.log(result);
+});
+// var data={
+//   id:10
+// };
+//
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+//
+// var decoded = jwt.verify(token,'123abc');
+// console.log(decoded);
 // var msg = 'I am ugbhgdhgss';
 //
 // var hash = SHA256(msg).toString();
